296746411e634e201e5ed476bbe199d43bbf26b8
Adrian Aguda <thediamondpegamer@gmail.com>
2022-04-15 21:56:22 +0800

    BalanceAdapter for Tablayout added

---
+++ app/src/main/java/com/hci3/aris/adapter/BalanceAdapter.java
@@ -0,0 +1,38 @@
+package com.hci3.aris.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.hci3.aris.ui.balance.BalanceFragment;
+import com.hci3.aris.ui.balance.Balance_fragment1;
+import com.hci3.aris.ui.balance.Enrollment_fragment2;
+import com.hci3.aris.ui.balance.Scholarship_fragment3;
+
+
+public class BalanceAdapter extends FragmentStateAdapter {
+
+    private final String[] titles = new String[]{"Balance", "Enrollment", "Scholarship"};
+
+    public BalanceAdapter(@NonNull BalanceFragment fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new Balance_fragment1();
+            case 1:
+                return new Enrollment_fragment2();
+            default:
+                return new Scholarship_fragment3();
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+}
--- app/src/main/java/com/hci3/aris/ui/balance/BalanceFragment.java
+++ app/src/main/java/com/hci3/aris/ui/balance/BalanceFragment.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.hci3.aris.R;
+import com.hci3.aris.adapter.BalanceAdapter;
 
 public class BalanceFragment extends Fragment {
 
@@ -21,6 +25,7 @@ public class BalanceFragment extends Fragment {
     public static BalanceFragment newInstance() {
         return new BalanceFragment();
     }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -31,6 +36,18 @@ public class BalanceFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(BalanceViewModel.class);
-    }
 
+        String[] titles = new String[]{"Balance", "Enrollment", "Scholarship"};
+        TabLayout tabLayout;
+        ViewPager2 viewPager2;
+
+        tabLayout = getView().findViewById(R.id.tabBar);
+        viewPager2 = getView().findViewById(R.id.viewPager);
+
+        BalanceAdapter adapter = new BalanceAdapter(this);
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2,((tab, position) ->
+                tab.setText(titles[position]))).attach();
+    }
 }
