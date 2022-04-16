145a197c0b20d9c6cde63aa60ffd3ed4ac678214
Yuki000h <103256391+Yuki000h@users.noreply.github.com>
2022-04-16 10:02:54 +0800

    Added TabLayout (#8)
    * BalanceAdapter for Tablayout added
    
    * BalanceAdapter for Tablayout added
    
    * BalanceAdapter for Tablayout added
    
    Co-authored-by: Joshua Hero <jhdcrux@outlook.com>

--- app/src/main/java/com/hci3/aris/ui/balance/BalanceFragment.java
+++ app/src/main/java/com/hci3/aris/ui/balance/BalanceFragment.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.hci3.aris.R;
+import com.hci3.aris.adapter.BalanceAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BalanceFragment extends Fragment {
 
@@ -27,9 +34,25 @@ public class BalanceFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_balance, container, false);
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-    }
 
+        List<String> titles = new ArrayList<>();
+        titles.add("Balance");
+        titles.add("Enrollment");
+        titles.add("Scholarship");
+
+
+        TabLayout tabLayout = view.findViewById(R.id.tabBar);
+        ViewPager2 viewPager2 = view.findViewById(R.id.viewPager);
+
+        BalanceAdapter adapter = new BalanceAdapter(getContext(), this , tabLayout.getTabCount());
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, (TabLayout.Tab tab, int position) ->
+                tab.setText(titles.get(position))).attach();
+
+    }
 }
